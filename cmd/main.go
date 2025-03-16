350784093330c062b5642f64df0e1bc9e7257021
emieli <emieli@example.com>
2025-03-16 21:11:20 +0100

    Put ListenAndService into go-routine
    Based on:
    https://medium.com/honestbee-tw-engineer/gracefully-shutdown-in-go-http-server-5f5e6b83da5a

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -14,10 +19,34 @@ func main() {
 
 	router.HandleFunc("GET /", getIndex)
 
-	err := http.ListenAndServe(":8000", router)
-	if err != nil {
-		log.Fatal(err)
+	srv := &http.Server{
+		Addr:    ":8000",
+		Handler: router,
 	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	log.Print("Server Started")
+
+	<-done
+	log.Print("Server Stopped")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		// extra handling here
+		cancel()
+	}()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+	log.Print("Server Exited Properly")
 }
 
 func getIndex(w http.ResponseWriter, r *http.Request) {
