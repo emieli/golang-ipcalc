c4871b08f9da8dbe91a173ab050a218d91f1916d
emieli <emieli@example.com>
2025-03-17 20:30:57 +0100

    Got templ/tailwind/wgo working

--- cmd/main.go
+++ cmd/main.go
@@ -37,7 +37,7 @@ func main() {
 	<-done
 	log.Print("Server Stopped")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer func() {
 		// extra handling here
 		cancel()
